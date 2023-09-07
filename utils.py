be13425246fa263fde338464e4b4861dc3b7a8d6
Sathees Paskaran <73420161+PaSathees@users.noreply.github.com>
2023-09-07 08:38:33 +0000

    Added download data function to utils.py

--- utils.py
+++ utils.py
@@ -11,6 +11,10 @@ Modified by:
 License: MIT
 """
 
+from pathlib import Path
+import os
+import zipfile
+import requests
 import torch
 
 
@@ -37,4 +41,47 @@ def walk_through_dir(dir_path):
       name of each subdirectory
     """
     for dirpath, dirnames, filenames in os.walk(dir_path):
-        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+        print(
+            f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'."
+        )
+
+
+def download_data(source: str, destination: str, remove_source: bool = True) -> Path:
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+
+    Returns:
+        pathlib.Path to downloaded data.
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+
+        # Download data
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as file:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            file.write(request.content)
+
+        # Unzip data
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...")
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+
+    return image_path
