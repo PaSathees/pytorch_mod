edca344075e8752d2b998a9c25cc9a902804a537
Sathees Paskaran <73420161+PaSathees@users.noreply.github.com>
2023-09-08 08:13:01 +0000

    Added ploting random image function

--- visualization.py
+++ visualization.py
@@ -11,6 +11,10 @@ Modified by:
 License: MIT
 """
 import matplotlib.pyplot as plt
+import random
+from PIL import Image
+import numpy as np
+
 
 def plot_curves(results):
     """Plots training curves of a results dictionary.
@@ -48,3 +52,37 @@ def plot_curves(results):
     plt.title("Accuracy")
     plt.xlabel("Epochs")
     plt.legend()
+
+
+NUM_PLOTS = 2
+
+
+def plot_random_images(
+    dir_path: str, file_pattern: str = "*/*/*.jpg", num_plots: int = 3
+):
+    """Plots random images from the given image directory.
+
+    Args:
+        dir_path (str): Image path of data image directory
+        file_pattern (str): Pattern and extension of the files. Default "*/*/*.jpg".
+        num_plots (int): Number of plots to plot, Default 3.
+    """
+    # Get image paths
+    image_paths = list(dir_path.glob(file_pattern))
+
+    for _ in range(num_plots):
+        # Get random images
+        random_img_path = random.choice(image_paths)
+        image_class = random_img_path.parent.stem
+        img = Image.open(random_img_path)
+
+        # Turn the image into an array
+        img_array = np.asarray(img)
+
+        # Plot the image with matplotlib
+        plt.figure(figsize=(10, 7))
+        plt.imshow(img_array)
+        plt.title(
+            f"Image class: {image_class} | Image shape: {img_array.shape} -> (HWC)"
+        )
+        plt.axis(False)
